981ecab9ecb28b2ecadaee27fd8e726b981ffb3f
dreamline91 <dreamline91@naver.com>
2017-09-16 01:36:11 +0900

    add init screen

---
+++ app/src/main/java/com/github/sensation/sensationjukebox/Play.java
@@ -0,0 +1,22 @@
+package com.github.sensation.sensationjukebox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dream on 2017-09-16.
+ */
+
+public class Play extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MapsActivity.class));
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+
+}
