c109ce6dd000ad5c547dc7a3662c3779ea80b918
wanttobeCE <pblwork1@gmail.com>
2017-09-17 02:07:18 +0900

    전기능 통합 버전

--- app/src/main/java/com/github/sensation/sensationjukebox/TopActivity.java
+++ app/src/main/java/com/github/sensation/sensationjukebox/TopActivity.java
@@ -16,7 +16,8 @@ public class TopActivity extends AppCompatActivity{
     private FloatingActionButton floatingActionButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
 
