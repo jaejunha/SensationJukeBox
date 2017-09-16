f2a784edafeacff9832690390806d8528f321916
dreamline91 <dreamline91@naver.com>
2017-09-16 11:39:52 +0900

    test youtubeDemo

--- app/src/main/java/com/github/sensation/sensationjukebox/youtube/YouTubeDemo.java
+++ app/src/main/java/com/github/sensation/sensationjukebox/youtube/YouTubeDemo.java
@@ -1,8 +1,11 @@
 package com.github.sensation.sensationjukebox.youtube;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.github.sensation.sensationjukebox.R;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 /*
@@ -26,27 +29,66 @@ import com.google.android.youtube.player.YouTubePlayerView;
  * displays a YouTube Video in a {@link YouTubePlayerView}.
  * <p>
  */
-public class YouTubeDemo extends YouTubeFailureRecoveryActivity {
+public class YouTubeDemo extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
 
-        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube);
-        youTubeView.initialize("AIzaSyD30ImlWgnzAFl38y2ZXZi9HJZzp5cNXE8", this);
+        getYouTubePlayerProvider().initialize("AIzaSyD30ImlWgnzAFl38y2ZXZi9HJZzp5cNXE8",this);
+
     }
 
+    /**
+     * 플레이어가 초기화될 때 호출됩니다.
+     * 매개변수
+     *
+     * provider YouTubePlayer를 초기화화는 데 사용된 제공자입니다.
+     * player 제공자에서 동영상 재생을 제어하는 데 사용할 수 있는 YouTubePlayer입니다
+     * wasRestored
+     *    YouTubePlayerView 또는 YouTubePlayerFragment가 상태를 복원하는 과정의 일부로서
+     *    플레이어가 이전에 저장된 상태에서 복원되었는지 여부입니다.
+     *    true는 일반적으로 사용자가 재생이 다시 시작될 거라고 예상하는 지점에서 재생을 다시 시작하고
+     *    새 동영상이 로드되지 않아야 함을 나타냅니다.
+     */
+
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
-                                        boolean wasRestored) {
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
         if (!wasRestored) {
-            player.cueVideo("wKJ9KzGQq0w");
+            player.cueVideo("IA1hox-v0jQ");  //http://www.youtube.com/watch?v=IA1hox-v0jQ
+
+            //cueVideo(String videoId)
+            //지정한 동영상의 미리보기 이미지를 로드하고 플레이어가 동영상을 재생하도록 준비하지만
+            //play()를 호출하기 전에는 동영상 스트림을 다운로드하지 않습니다.
+            //https://developers.google.com/youtube/android/player/reference/com/google/android/youtube/player/YouTubePlayer
         }
     }
 
+    //플레이어가 초기화되지 못할 때 호출됩니다.
     @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+           //
+        }
+    }
+
     protected YouTubePlayer.Provider getYouTubePlayerProvider() {
         return (YouTubePlayerView) findViewById(R.id.youtube);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize("AIzaSyD30ImlWgnzAFl38y2ZXZi9HJZzp5cNXE8", this);
+        }
     }
 }
