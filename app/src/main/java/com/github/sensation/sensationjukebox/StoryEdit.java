52f0d7fe13672a6d0285f3e4da7e7e5ce097c662
Dong Hee <ehd8266@naver.com>
2017-09-16 13:12:10 +0900

    Set storyedit button option

--- app/src/main/java/com/github/sensation/sensationjukebox/StoryEdit.java
+++ app/src/main/java/com/github/sensation/sensationjukebox/StoryEdit.java
@@ -2,12 +2,26 @@ package com.github.sensation.sensationjukebox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.gc.materialdesign.views.ButtonRectangle;
 
 public class StoryEdit extends AppCompatActivity {
+
+    private ButtonRectangle cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_edit);
+
+        cancel = (ButtonRectangle)findViewById(R.id.Cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
