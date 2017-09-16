ee0e2f549dea7449050bb55476debb407245ea25
Unknown <thswnsxor123@naver.com>
2017-09-16 15:38:19 +0900

    commit error issue

--- app/src/main/java/com/github/sensation/sensationjukebox/StoryEdit.java
+++ app/src/main/java/com/github/sensation/sensationjukebox/StoryEdit.java
@@ -7,16 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import com.gc.materialdesign.views.ButtonRectangle;
 
-<<<<<<< HEAD
-import com.gc.materialdesign.views.ButtonRectangle;
-
-public class StoryEdit extends AppCompatActivity implements View.OnClickListener{
-    Button SongSearch_Button;
-=======
 public class StoryEdit extends AppCompatActivity implements View.OnClickListener{
     Button SongSearch_Button;
 
->>>>>>> master
     private ButtonRectangle cancel;
 
     @Override
